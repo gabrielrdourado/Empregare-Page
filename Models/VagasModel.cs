586a4999d8555d905c803652d8665a76c92c3af0
Gabriel Ribeiro Dourado <dourado.gabrielribeirodourado@gmail.com>
2021-02-09 21:08:05 -0300

    Add project files.

---
+++ Models/VagasModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace empregare.Models
+{
+    public class VagasModel
+    {
+        public string Cargo { get; set; }
+
+        public string Cidade { get; set; }
+
+        public double Salario { get; set; }
+    }
+}
---
+++ Views/Shared/Components/Vagas/VagasViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using empregare.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace empregare.Views.Shared.Components.Vagas
+{
+    public class VagasViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var vagas = new List<VagasModel>();
+
+            vagas.Add(new VagasModel {Cargo = "Vendedor", Cidade = "Ribeirão Preto", Salario = 1500.00});
+            vagas.Add(new VagasModel { Cargo = "Programador", Cidade = "Serrana", Salario = 3570.50 });
+            vagas.Add(new VagasModel { Cargo = "Design", Cidade = "Campinas", Salario = 1720.00 });
+            vagas.Add(new VagasModel { Cargo = "Auxiliar", Cidade = "Ribeirão Preto", Salario = 1356.40 });
+            vagas.Add(new VagasModel { Cargo = "Engenheiro", Cidade = "São Paulo", Salario = 3700.00 });
+            vagas.Add(new VagasModel { Cargo = "Administrador", Cidade = "Campinas", Salario = 1400.00 });
+            vagas.Add(new VagasModel { Cargo = "Vendedor Externo", Cidade = "Jardinópolis", Salario = 2200.00 });
+            vagas.Add(new VagasModel { Cargo = "Gerente", Cidade = "São Paulo", Salario = 4730.00 });
+
+
+            return View(vagas);
+        }
+    }
+}
